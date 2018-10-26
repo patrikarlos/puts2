e948f6ffd7a5323366784839dbbd2350a8596e00
vasudha2796 <vasudha.dasari@yahoo.com>
2018-10-26 22:56:34 +0200

    integer operation are added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-
+from fractions import Fraction
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +13,17 @@ def addition():
     value2=request.args.get('B',default = 0, type = int)
     result=value1+value2
     return '%d \n' % result
+
+@app.route('/addr')
+def rationaladdition():
+    value1=request.args.get('A',default = 0, type = str)
+    num1=str(value1).split('/')
+    n1,d1=[int(x) for x in num1]
+    value2=request.args.get('B',default = 0, type = str)
+    num1=str(value2).split('/')
+    n2,d2=[int(x) for x in num1]
+    result=str(Fraction(n1,d1)+Fraction(n2,d2))
+    return result
 
 
 if __name__ == "__main__":
