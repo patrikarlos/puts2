b89ab64bccdebffc2718ee86b71a40c62bc09ac3
vasudha2796 <vasudha.dasari@yahoo.com>
2018-10-27 02:10:13 +0200

    rational numbers are added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from fractions import Fraction
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -38,6 +39,56 @@ def int_division():
     value2=request.args.get('B',default = 0, type = float)
     result=value1/value2
     return '%.2f \n' % result
+
+@app.route('/addr')
+def rat_addition():
+    value1=request.args.get('A',default = 0, type = str)
+    num1=str(value1).split('/')
+    n1,d1=[int(x) for x in num1]
+    value2=request.args.get('B',default = 0, type = str)
+    num1=str(value2).split('/')
+    n2,d2=[int(x) for x in num1]
+    result=str(Fraction(n1,d1)+Fraction(n2,d2))
+    return result
+
+
+@app.route('/subr')
+def rat_subtraction():
+    value1=request.args.get('A',default = 0, type = str)
+    num1=str(value1).split('/')
+    n1,d1=[int(x) for x in num1]
+    value2=request.args.get('B',default = 0, type = str)
+    num1=str(value2).split('/')
+    n2,d2=[int(x) for x in num1]
+    result=str(Fraction(n1,d1)-Fraction(n2,d2))
+    return result
+
+
+@app.route('/mulr')
+def rat_multiplication():
+    value1=request.args.get('A',default = 0, type = str)
+    num1=str(value1).split('/')
+    n1,d1=[int(x) for x in num1]
+    value2=request.args.get('B',default = 0, type = str)
+    num1=str(value2).split('/')
+    n2,d2=[int(x) for x in num1]
+    result=str(Fraction(n1,d1)*Fraction(n2,d2))
+    return result
+
+@app.route('/divr')
+def rat_division():
+    value1=request.args.get('A',default = 0, type = str)
+    num1=str(value1).split('/')
+    n1,d1=[int(x) for x in num1]
+    value2=request.args.get('B',default = 0, type = str)
+    num1=str(value2).split('/')
+    n2,d2=[int(x) for x in num1]
+    result=str(Fraction(n1,d1)/Fraction(n2,d2))
+    return result
+
+
+
+
 
 
 if __name__ == "__main__":
