733c2507ac4ee28fccf7d31c1fa0d31c6058be9d
sumanth8ch <chsm17@student.bth.se>
2019-01-25 11:16:10 +0100

    mul function added

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def index():
     return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
+@app.route('/mul')
 def addition():
     value1=request.args.get('A',default = 0, type = Fraction)
     value2=request.args.get('B',default = 0, type = Fraction)
     x = Fraction(value1)
     y= Fraction(value2)
-    result=value1+value2
+    result=value1*value2
     temp = str(result).split('/')
     if len(temp) == 2:
         d = int(temp[0])/int(temp[1])
--- test.py
+++ test.py
@@ -8,18 +8,18 @@ class MyTestCase(unittest.TestCase):
         main.app.testing = True
         self.app = main.app.test_client()
 
-    def test_addint(self):
-        rv =  self.app.get('/add?A=2&B=3')
-        self.assertEqual(b'5 \n', rv.data)
-        self.assertNotEqual(b'6.000 \n',rv.data)
-    def test_addfloat(self):
-        rv =  self.app.get('/add?A=2.3&B=3.3')
-        self.assertEqual(b'5.6 \n', rv.data)
-    def test_addfrac(self):
-        rv =  self.app.get('/add?A=2/3&B=3/3')
-        self.assertEqual(b'1.6666666666666667 \n', rv.data)
-    def test_addneg(self):
-        rv =  self.app.get('/add?A=2.3&B=-3.3')
-        self.assertEqual(b'-1 \n', rv.data)
+    def test_mulint(self):
+        rv =  self.app.get('/mul?A=2&B=3')
+        self.assertEqual(b'6 \n', rv.data)
+        self.assertNotEqual(b'5.000 \n',rv.data)
+    def test_mulfloat(self):
+        rv =  self.app.get('/mul?A=2.3&B=3.3')
+        self.assertEqual(b'7.59 \n', rv.data)
+    def test_mulfrac(self):
+        rv =  self.app.get('/mul?A=2/3&B=3/3')
+        self.assertEqual(b'0.6666666666666666 \n', rv.data)
+    def test_mulneg(self):
+        rv =  self.app.get('/mul?A=-2.3&B=-3.3')
+        self.assertEqual(b'7.59 \n', rv.data)
 if __name__ == '__main__':
     unittest.main()
