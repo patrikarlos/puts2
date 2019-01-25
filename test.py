7b4efff99d0d995068bb9f13414d0dc0075341c9
sumanth8ch <chsm17@student.bth.se>
2019-01-25 10:52:11 +0100

    test file added

---
+++ test.py
@@ -0,0 +1,25 @@
+import main
+import unittest
+import subprocess
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def test_addint(self):
+        rv =  self.app.get('/add?A=2&B=3')
+        self.assertEqual(b'5 \n', rv.data)
+        self.assertNotEqual(b'6.000 \n',rv.data)
+    def test_addfloat(self):
+        rv =  self.app.get('/add?A=2.3&B=3.3')
+        self.assertEqual(b'5.6 \n', rv.data)
+    def test_addfrac(self):
+        rv =  self.app.get('/add?A=2/3&B=3/3')
+        self.assertEqual(b'1.6666666666666667 \n', rv.data)
+    def test_addneg(self):
+        rv =  self.app.get('/add?A=2.3&B=-3.3')
+        self.assertEqual(b'-1 \n', rv.data)
+if __name__ == '__main__':
+    unittest.main()
