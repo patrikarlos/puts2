423ed30ec9d6333e3a049b0dd7654273dc142215
sroopsai <roopsai.work@gmail.com>
2019-02-20 15:11:14 +0530

    Added integer operations

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
+@app.route('/add',methods=['POST','GET'])
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
+    value1=int(request.form['A'])
+    value2=int(request.form['B'])
     result=value1+value2
     return '%d \n' % result
+
+@app.route('/sub',methods=['POST','GET'])
+def subtraction():
+    value1=int(request.form['A'])
+    value2=int(request.form['B'])
+    result=value1-value2
+    return '%d \n' % result
+
+@app.route('/mul',methods=['POST','GET'])
+def multiplication():
+    value1=int(request.form['A'])
+    value2=int(request.form['B'])
+    result=value1*value2
+    return '%d \n' % result
+
+@app.route('/div',methods=['POST','GET'])
+def division():
+    value1=int(request.form['A'])
+    value2=int(request.form['B'])
+    result=value1/value2
+    return '%d \n' % result
+
+
 
 
 if __name__ == "__main__":
