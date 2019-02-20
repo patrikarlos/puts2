a4cd81896c8323c45a025bf88f95b2a34995a4b4
sroopsai <roopsai.work@gmail.com>
2019-02-20 15:36:12 +0530

    Added code to handle rational operations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from sympy import Rational
 
 app = Flask(__name__)
 
@@ -9,32 +10,51 @@ def index():
 
 @app.route('/add',methods=['POST','GET'])
 def addition():
-    value1=int(request.form['A'])
-    value2=int(request.form['B'])
-    result=value1+value2
-    return '%d \n' % result
+    value1=Rational(request.form['A'])
+    value2=Rational(request.form['B'])
+
+    result=float(value1+value2)
+    if result.is_integer():
+        return str(int(result))+'\n'
+    else:
+
+        return str(result)+'\n'
 
 @app.route('/sub',methods=['POST','GET'])
 def subtraction():
-    value1=int(request.form['A'])
-    value2=int(request.form['B'])
-    result=value1-value2
-    return '%d \n' % result
+    value1=Rational(request.form['A'])
+    value2=Rational(request.form['B'])
+
+    result=float(value1-value2)
+    if result.is_integer():
+        return str(int(result))+'\n'
+    else:
+
+        return str(result)+'\n'
 
 @app.route('/mul',methods=['POST','GET'])
 def multiplication():
-    value1=int(request.form['A'])
-    value2=int(request.form['B'])
-    result=value1*value2
-    return '%d \n' % result
+    value1=Rational(request.form['A'])
+    value2=Rational(request.form['B'])
+
+    result=float(value1*value2)
+    if result.is_integer():
+        return str(int(result))+'\n'
+    else:
+
+        return str(result)+'\n'
 
 @app.route('/div',methods=['POST','GET'])
 def division():
-    value1=int(request.form['A'])
-    value2=int(request.form['B'])
-    result=value1/value2
-    return '%d \n' % result
+    value1=Rational(request.form['A'])
+    value2=Rational(request.form['B'])
 
+    result=float(value1/value2)
+    if result.is_integer():
+        return str(int(result))+'\n'
+    else:
+
+        return str(result)+'\n'
 
 
 
