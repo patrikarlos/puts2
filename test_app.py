f2f3e1715b2f3218ec9db65b665031e9ddf01fcf
Babar Khan <babarkhan2491@gmail.com>
2019-09-29 00:26:11 +0100

    change existing test cases to comply with rational numbers

--- test_app.py
+++ test_app.py
@@ -11,22 +11,22 @@ def test_index_page():
 def test_addition():
     r = requests.get(url + '/add?A=10&B=20')
     assert r.status_code == 200
-    assert r.text == "30 \n"
+    assert r.text == "30.000 \n"
 
 
 def test_subtraction():
     r = requests.get(url + '/subtract?A=10&B=20')
     assert r.status_code == 200
-    assert r.text == "-10 \n"
+    assert r.text == "-10.000 \n"
 
 
 def test_multiplication():
     r = requests.get(url + '/multiply?A=10&B=20')
     assert r.status_code == 200
-    assert r.text == "200 \n"
+    assert r.text == "200.000 \n"
 
 
 def test_division():
     r = requests.get(url + '/divide?A=10&B=2')
     assert r.status_code == 200
-    assert r.text == "5 \n"
+    assert r.text == "5.000 \n"
