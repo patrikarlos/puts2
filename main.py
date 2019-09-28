ba4b54a055fac8daddc47cbbab8aadc475730b4c
Babar Khan <babarkhan2491@gmail.com>
2019-09-29 00:05:10 +0100

    add API for division

--- main.py
+++ main.py
@@ -31,5 +31,13 @@ def multiplication():
     result = value1*value2
     return '%d \n' % result
 
+
+@app.route('/divide')
+def division():
+    value1 = request.args.get('A', default=0, type=int)
+    value2 = request.args.get('B', default=0, type=int)
+    result = value1/value2
+    return '%d \n' % result
+
 if __name__ == "__main__":
     app.run()
