2f0cff888fd34296131eb7aa934789edb906b93b
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-04-23 18:29:01 +0200

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from flask import Flask, request
-
+from fractions import Fraction
+from decimal import Decimal
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-
+    return 'write values of A and B'
+	#return 'write in the format of url/operation? A=value & B=value'
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
-
+    try:
+        arg1 = request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError:
+        arg1 = 'None'
+    try:
+        arg2 = request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError:
+        arg2 = 'None'
+    if arg1=='None' or arg2 == 'None':
+        return'None'
+    else:
+        X= Fraction(arg1)
+        Y = Fraction(arg2)
+        Z = X+Y
+        return str(round(float(Z),3))
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
