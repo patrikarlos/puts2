0ffed4cc4c591d50ab290f9ba202ce8ad751ed75
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-04-24 12:02:23 +0200

    first commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from decimal import Decimal
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'write values of A and B'
-	#return 'write in the format of url/operation? A=value & B=value'
+    return 'write values of A and B in the format of url/operation? A=arg1 & B=arg2'
+	
 @app.route('/add')
 def addition():
     try:
@@ -17,11 +17,31 @@ def addition():
     except ZeroDivisionError:
         arg2 = 'None'
     if arg1=='None' or arg2 == 'None':
-        return'None'
+        return'Error:check the args given above'
     else:
         X= Fraction(arg1)
         Y = Fraction(arg2)
         Z = X+Y
         return str(round(float(Z),3))
+
+@app.route('/sub')
+def substraction():
+    try:
+        arg1 = request.args.get('A' , default = 0, type = Fraction)
+    except ZeroDivisionError:
+        arg1 = 'None'
+    try:
+        arg2 = request.args.get('B',default =0, type = Fraction)
+    except ZeroDivisionError:
+        arg2 = 'None'
+    if arg1 == 'None' or arg2 == 'None':
+        return'Error:check the args given above'
+    else:
+        X= Fraction(arg1)
+        Y = Fraction(arg2)
+        Z = X-Y
+        return str(round(float(Z),3))
+        
+    
 if __name__ == "__main__":
     app.run(debug=True)
