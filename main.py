f2ac333ad34701a74dac1842f7b384b5c9d4524b
vamsivenkat987 <55131501+vamsivenkat987@users.noreply.github.com>
2020-04-29 11:40:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from fractions import Fraction
+from decimal import Decimal
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'gives the addition between arg1 and arg2'
+
+
+@app.route('/add')
+def addition():
+    arg1=request.args.get('A',default = 0, type = Fraction)
+    arg2=request.args.get('B',default = 0, type = Fraction)
+    X= Fraction(arg1)
+    Y= Fraction(arg2)
+    Z=X+Y
+   
+    result = str(Z).split('/')
+    if len(result) == 2:
+        first_arg = int(result[0])/int(result[1])
+        second_arg = str(first_arg).split('.')
+        if second_arg[1] == '0':
+            return '$ \n'% second_arg[0]
+        else:
+            return '$ \n' % first_arg
+    else:
+        result1=str(Z).split('.')
+        return '$ \n'% result1[0]
+
+
+if __name__ == "__main__":
+    app.run()
