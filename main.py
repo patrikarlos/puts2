d5f9258018952186a68103d712ff67dce65887bc
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 13:51:35 +0200

    updating test for addition.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def index():
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = Fraction)
-    value2=request.args.get('B',default = 0, type = Fraction)
-    data=value1+value2
+    Value1=request.args.get('A',default = 0, type = Fraction)
+    Value2=request.args.get('B',default = 0, type = Fraction)
+    data=Value1+Value2
     result= float(data)
     return '%d \n' % result
 
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import unittest
-import requests
+from flask import Flask, request
 import json
 from fractions import Fraction
 
-value1=[5.556,7,2/3,9.1]
-value2=[6.555,14,3/2,1.9]
+V1=[5.556,7,2/3,9.1]
+V2=[6.555,14,3/2,1.9]
 
 add_script=[]
 add_test=[]
-for i in range (0,len(value1)):
-    add_from_test=value1[i] + value2[i]
+for i in range (0,len(V1)):
+    add_from_test=V1[i] + V2[i]
     add_test.append(round(add_from_test,3))
     PARAM= {"A":Fraction(value1[i]),"B":Fraction(value2[i])}
-    add_from_url='http://127.0.0.1:5000/add'
-    req=requests.get(add_from_URL,params=PARAM)
+    url_add = 'http://127.0.0.1:5000/add'
+    req=request.get(url_add ,params=PARAM)
     data=req.json()
-    add_script.append(float(data,3))
+    add_script.append(round(data,3))
 
 if add_script[i]==add_test[i]:
-    print "sucessfully tested"
+    print ("sucessfully tested")
 else:
-    print "testing failed"
+    print ("testing failed")
