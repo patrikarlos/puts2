5ff757db4252be3f534987c247c597753e8e979b
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 11:00:20 +0200

    updating test.py in order to perform addtion operation.

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import requests
 import json
 from fractions import Fraction
 
-value1=[]
-value2=[]
+value1=[5.556,7,2/3,9.1]
+value2=[6.555,14,3/2,1.9]
 
-script=[]
-test=[]
+add_script=[]
+add_test=[]
+for i in range (0,len(value1)):
+    add_from_test=value1[i] + value2[i]
+    add_test.append(round(add_from_test,3))
+    
