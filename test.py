2717672a0d4a378230b00822039bb51448c971dd
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 02:18:51 +0200

    testing addition operation for script and URL and comparing the results.

--- test.py
+++ test.py
@@ -11,3 +11,13 @@ add_test=[]
 for i in range (0,len(value1)):
     add_from_test=value1[i] + value2[i]
     add_test.append(round(add_from_test,3))
+    PARAM= {"A":Fraction(value1[i]),"B":Fraction(value2[i])}
+    add_from_url='http://127.0.0.1:5000/add'
+    req=requests.get(add_from_URL,params=PARAM)
+    data=req.json()
+    add_script.append(float(data,3))
+
+if add_script[i]==add_test[i]:
+    print "sucessfully tested"
+else:
+    print "testing failed"
