ce0dae338aa5e50fad3d526e4635b2aaef93f9bb
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 14:10:59 +0200

    modified code for the conficts.

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ add_script=[]
 add_test=[]
 for i in range (0,len(V1)):
     add_from_test=V1[i] + V2[i]
-    add_test.append((add_from_test,3))
+    add_test.append(round(add_from_test,3))
     PARAM= {"A":Fraction(V1[i]),"B":Fraction(V2[i])}
-    url_add = 'http://127.0.0.1:5000/'
+    url_add = 'http://127.0.0.1:5000/add'
     req=request.get(url_add ,params=PARAM)
     data=req.json()
-    add_script.append((data))
+    add_script.append(round(data,3))
 
 if add_script[i]==add_test[i]:
     print ("sucessfully tested")
