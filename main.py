dd340e0e8bd3b58c0401d74a06e6cec4109f7b03
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 14:44:31 +0200

    modifed main.py inorder to do multiplication operatrion.

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ def addition():
     result= float(data)
     return '%d \n' % result
 
+@app.route('/sub')
+def subtraction():
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('B',default = 0, type = Fraction)
+    data=value1-value2
+    result= float(data)
+    return '%d \n' % result
+
+@app.route('/multiplication')
+def multiplication()
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('B',default = 0, type = Fraction)
+    data=value1*value2
+    result= float(data)
+    return '%d \n' % result
+
+
 
 if __name__ == "__main__":
     app.run()
