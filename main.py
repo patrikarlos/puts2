40b9bca10ca9a250eaa23f203d37e5f3a6898e1f
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 10:42:47 +0200

    fixing conflicts.

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    return 'Usage;\n<Operation>?A=<V1>&B=<V2>\n'
 
 
 @app.route('/add')
 def addition():
     value1=request.args.get('A',default = 0, type = Fraction)
     value2=request.args.get('B',default = 0, type = Fraction)
-    C=value1+value2
-    result= float(c)
+    C=v1+v2
+    result= float(C)
     return '%d \n' % result
 
 
