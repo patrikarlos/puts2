fa71a350928bb658786a03603f108b867dab301b
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-06-01 14:08:34 +0200

    updated conflicts

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 import unittest
-import requests
+from flask import Flask, request
 import json
 from fractions import Fraction
 
-value1=[5.556,7,2/3,9.1]
-value2=[6.555,14,3/2,1.9]
+V1=[5.556,7,2/3,9.1]
+V2=[6.555,14,3/2,1.9]
 
 add_script=[]
 add_test=[]
-for i in range (0,len(value1)):
-    add_from_test=value1[i] + value2[i]
-    add_test.append(round(add_from_test,3))
-    
+for i in range (0,len(V1)):
+    add_from_test=V1[i] + V2[i]
+    add_test.append((add_from_test,3))
+    PARAM= {"A":Fraction(V1[i]),"B":Fraction(V2[i])}
+    url_add = 'http://127.0.0.1:5000/'
+    req=request.get(url_add ,params=PARAM)
+    data=req.json()
+    add_script.append((data))
+
+if add_script[i]==add_test[i]:
+    print ("sucessfully tested")
+else:
+    print ("testing failed")
