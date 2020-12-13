2f23ddf6ee0bdd97c5059c90d02702fe922dcea6
deepika-challa <55228517+deepika-challa@users.noreply.github.com>
2020-12-14 00:29:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,101 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+
+@app.route('/average', methods=['GET'])
+def average():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/mean', methods=['GET'])
+def mean():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/avg', methods=['GET'])
+def avg():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/median', methods=['GET'])
+def median():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    med = statistics.median(vector)
+    return str(med)
+
+@app.route('/maximum', methods=['GET'])
+def maximum():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    m = max(vector)
+    return str(m)
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(port = 5000 ,debug=True)
