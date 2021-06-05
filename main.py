b0114c99cb6ec6a72cd0b8f8be00dfe4658460d0
deepika-challa <55228517+deepika-challa@users.noreply.github.com>
2021-06-05 10:28:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,204 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+
+@app.route('/average', methods=['GET'])
+def average():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/mean', methods=['GET'])
+def mean():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/avg', methods=['GET'])
+def avg():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+    avg = sum / len(vector)
+    return str(avg)
+
+@app.route('/median', methods=['GET'])
+def median():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    med = statistics.median(vector)
+    return str(med)
+
+@app.route('/maximum', methods=['GET'])
+def maximum():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    m = max(vector)
+    return str(m)
+
+@app.route('/minimum', methods=['GET'])
+def minimum():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    m = min(vector)
+    return str(m)
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+
+@app.route('/add', methods=['GET'])
+def add():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 0
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum + n
+
+    return str(sum)
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/mul', methods=['GET'])
+def mul():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 1
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    for n in vector :
+        sum = sum * n
+
+    return str(sum)
+
+
+
+@app.route('/div', methods=['GET'])
+def div():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 1
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    if(len(vector)==2) :
+        return str(vector[0]/vector[1])
+    else :
+        return str("INPUT ONLY 2 NUMBERS BECAUSE DIVISION OPERATION WILL BE DONE ON ONLY 2 OPERANDS")
+
+
+
+@app.route('/sub', methods=['GET'])
+def sub():
+    data = request.args.to_dict()
+    print(data)
+    string = data['X']
+    vector = string.split(",")
+    sum = 1
+    for i in range(len(vector)) :
+        if vector[i].find('.') == -1 :
+            vector[i] = int(vector[i])
+        else :
+            vector[i]= float(vector[i])
+
+    print(vector)
+    if(len(vector)==2) :
+        return str(vector[0]-vector[1])
+    else :
+        return str("INPUT ONLY 2 NUMBERS BECAUSE SUBSTRACTION OPERATION WILL BE DONE ON ONLY 2 OPERANDS")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port = 5000 ,debug=True)
